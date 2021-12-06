b661b3d1d446ff1df40a9ac833a5226146f330b5
Ali Moeinian <Eng.alimoeinian1379@gmail.com>
2021-12-06 23:03:13 +0100

    if statement to check situation of math, added

--- ListStudents.py
+++ ListStudents.py
@@ -1,3 +1,8 @@
 list_students=['Ali','mohammad','vahid','karim','maryam','niki']
 if 'Ali' and 'mohammad' in list_students:
     print("Those students are in the class.")
+grade_ali = int(input('please enter grade of math : '))
+if grade_ali > 10:
+    print('you pass the math.')
+else:
+    print('you failed.')    
